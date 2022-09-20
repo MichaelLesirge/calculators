7175ec5709564f4d7e7db31656dc8e65e7dfa394
Michael Lesirge <michaellesirge@gmail.com>
2022-09-20 13:53:51 -0700

    create midpoint formula

---
+++ random-calculators/2022-2023/mid.py
@@ -0,0 +1,20 @@
+def sq(x):
+    return (x)**2
+
+def sqrt(x):
+    if x < 0:
+        raise Exception("Can no square negitive number %s" % x)
+    return (x)**0.5
+
+while True:
+    x1 = float(input("x1:"))
+    y1 = float(input("y1:"))
+    x2 = float(input("x2:"))
+    y2 = float(input("y2:"))
+
+    print("sqrt(sq(%s-%s) + sq(%s-%s))" % (x1, x2, y1, y2))
+    print("sqrt(sq(%s) + sq(%s))" % (x1-x2, y1-y2))
+    print("sqrt(%s + %s)" % (sq(x1-x2), sq(y1-y2)))
+    print("sqrt(%s)" % (sq(x1-x2) + sq(y1-y2)))
+    print("%s" % sqrt(sq(x1-x2) + sq(y1-y2)))
+    print()
