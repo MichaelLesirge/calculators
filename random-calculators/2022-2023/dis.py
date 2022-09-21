22f8e60ebf0121f26683208d18b8eca996358497
Michael Lesirge <michaellesirge@gmail.com>
2022-09-20 21:25:50 -0700

    improve both calcultors with sint

--- random-calculators/2022-2023/dis.py
+++ random-calculators/2022-2023/dis.py
@@ -6,15 +6,25 @@ def sqrt(x):
         raise Exception("Can not square root negitive number %s" % x)
     return (x)**0.5
 
+def sint(x):
+    x = float(x)
+    if str(x)[-2:] == ".0":
+        return int(x)
+    return x
+
 while True:
-    x1 = float(input("x1: "))
-    y1 = float(input("y1: "))
-    x2 = float(input("x2: "))
-    y2 = float(input("y2: "))
+    x1 = sint(input("x1: "))
+    y1 = sint(input("y1: "))
+    x2 = sint(input("x2: "))
+    y2 = sint(input("y2: "))
 
     print("sqrt(sq(%s - %s) + sq(%s - %s))" % (x1, x2, y1, y2))
-    print("sqrt(sq(%s) + sq(%s))" % (x1-x2, y1-y2))
-    print("sqrt(%s + %s)" % (sq(x1-x2), sq(y1-y2)))
-    print("sqrt(%s)" % (sq(x1-x2) + sq(y1-y2)))
-    print("%s" % sqrt(sq(x1-x2) + sq(y1-y2)))
+    x, y = sint(x1-x2), sint(y1-y2)
+    print("sqrt(sq(%s) + sq(%s))" % (x, y))
+    x, y = sint(sq(x1-x2)), sint(sq(y1-y2))
+    print("sqrt(%s + %s)" % (x, y))
+    a = sint(sq(x1-x2) + sq(y1-y2))
+    print("sqrt(%s)" % (a))
+    a = sint(sqrt(a))
+    print("%s" % a)
     print()
--- random-calculators/2022-2023/mid.py
+++ random-calculators/2022-2023/mid.py
@@ -1,10 +1,18 @@
+def sint(x):
+    x = float(x)
+    if str(x)[-2:] == ".0":
+        return int(x)
+    return x
+
 while True:
-    x1 = float(input("x1: "))
-    y1 = float(input("y1: "))
-    x2 = float(input("x2: "))
-    y2 = float(input("y2: "))
+    x1 = sint(input("x1: "))
+    y1 = sint(input("y1: "))
+    x2 = sint(input("x2: "))
+    y2 = sint(input("y2: "))
 
     print("((%s + %s) / 2, (%s + %s) / 2)" % (x1, x2, y1, y2))
-    print("(%s / 2, %s / 2)" % (x1 + x2, y1 + y2))
-    print("(%s, %s)" % ((x1 + x2)/2, (y1 + y2)/2))
+    x, y = sint(x1 + x2), sint(y1 + y2)
+    print("(%s / 2, %s / 2)" % (x, y))
+    x, y = sint(x / 2), sint(x / 2) 
+    print("(%s, %s)" % (x, y))
     print()
