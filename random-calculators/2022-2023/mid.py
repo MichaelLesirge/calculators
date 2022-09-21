f6a10c716e7a0d283efcdccfddf446135f490d0d
Michael Lesirge <michaellesirge@gmail.com>
2022-09-20 21:05:59 -0700

    add space to input

--- random-calculators/2022-2023/mid.py
+++ random-calculators/2022-2023/mid.py
@@ -7,10 +7,10 @@ def sqrt(x):
     return (x)**0.5
 
 while True:
-    x1 = float(input("x1:"))
-    y1 = float(input("y1:"))
-    x2 = float(input("x2:"))
-    y2 = float(input("y2:"))
+    x1 = float(input("x1: "))
+    y1 = float(input("y1: "))
+    x2 = float(input("x2: "))
+    y2 = float(input("y2: "))
 
     print("sqrt(sq(%s-%s) + sq(%s-%s))" % (x1, x2, y1, y2))
     print("sqrt(sq(%s) + sq(%s))" % (x1-x2, y1-y2))
