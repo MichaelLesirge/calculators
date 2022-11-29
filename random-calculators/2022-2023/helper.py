3f60263cc7b0581b54cc77718a541dc15150bed0
Michael Lesirge <michaellesirge@gmail.com>
2022-11-28 20:05:41 -0800

    improve how fractions work

--- random-calculators/2022-2023/helper.py
+++ random-calculators/2022-2023/helper.py
@@ -3,3 +3,14 @@ def sint(x):
     if str(x)[-2:] == ".0" or x % 1 <= 0.00000000000001:
         return int(x)
     return x
+
+
+def gcd(x, y):
+	while y:
+		x, y = y, x % y
+	return abs(x)
+
+def simplify(x, y):
+  divisor = gcd(x, y)
+  return (x / divisor, y / divisor)
+    
--- random-calculators/2022-2023/slp.py
+++ random-calculators/2022-2023/slp.py
@@ -1,14 +1,24 @@
-from helper import sint
-
+from helper import sint, gcd
 
 def slp(x1, y1, x2, y2):
     print("(%s - %s) / (%s - %s)" % (y2, y1, x2, x1))
-    t, b = y2 - y1, x2 - x1
-    print("%s / %s" % (t, b))
+    t, b = sint(y2 - y1), sint(x2 - x1)
+    print("(%s) / (%s)" % (t, b))
     try:
-        s = t / b
+        s = sint(t / b)
     except ZeroDivisionError:
         s = "undifined"
+
+    if t < 0 and b < 0:
+        t, b = abs(t), abs(b)
+    
+    print("%s / %s" % (t, b))
+
+    if isinstance(t, int) and isinstance(b, int):
+        d = sint(gcd(abs(t), abs(b)))
+        if d != 1:
+            print("%s / %s gcd: %s" % (sint(t / d), sint(b / d), d))
+
     print(s)
 
     return s
