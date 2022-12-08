fbe12da8e91070375fa698fe2e42888480f7b884
Michael Lesirge <michaellesirge@gmail.com>
2022-12-07 19:58:07 -0800

    create make line from point

---
+++ random-calculators/2022-2023/gline.py
@@ -0,0 +1,30 @@
+from helper import get_point, sint, simplify
+
+while True:
+    m = sint(eval(input("m: ")))
+
+    x, y = get_point("", "p")
+
+    mode = input("enter mode e/a/b: ")
+
+    if mode == "b":
+        parallel = True
+        perpendicular = True
+    elif mode == "a":
+        parallel = True
+        perpendicular = False
+    elif mode == "e":
+        parallel = False
+        perpendicular = True
+
+    if parallel:
+        b = sint(y - (m * x))
+        print("parallel: y = %sx + %s" % (m, b))
+    if perpendicular:
+        m = sint(-1 / m)
+        b = y - (m * x)
+
+        tb, bb = simplify(-1, m)
+        print("b = %s/%s" % (tb, bb))
+        print("perpendicular: y = %sx + %s" % (m, b))
+    
