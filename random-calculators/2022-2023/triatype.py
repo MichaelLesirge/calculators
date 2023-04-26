342c767f082491a79c774b50fed26019a3ab7ddb
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2023-04-26 10:27:04 -0700

    make work better

--- random-calculators/2022-2023/triatype.py
+++ random-calculators/2022-2023/triatype.py
@@ -1,15 +1,30 @@
 from helper import get_sint
 
-a = get_sint("a")
-b = get_sint("b")
-c = get_sint("c")
+while True:
+    a = get_sint("a")
+    b = get_sint("b")
+    c = get_sint("c")
 
-l = a ** 2 + b ** 2
-r = c ** 2
+    l = a ** 2 + b ** 2
+    r = c ** 2
+    
+    if c != max(a, b, c):
+        print("C is not the highest value")        
+    if not (a + b > c and a + c > b and b + c > a):
+        print("Triangle is not valid")
+    
+    print()
 
-if l < r:
-    print("obtuse")
-elif l > r:
-    print("acute")
-else:
-    print("right")
+    if l > r:
+        print("%s^2 + %s^2 > %s^2" % (a, b, c))
+        print("%s > %s" % (l, r))
+        print("acute")
+    elif l < r:
+        print("%s^2 + %s^2 < %s^2" % (a, b, c))
+        print("%s < %s" % (l, r))
+        print("obtuse")
+    else:
+        print("%s^2 + %s^2 = %s^2" % (a, b, c))
+        print("%s = %s" % (l, r))
+        print("right")
+    print()
