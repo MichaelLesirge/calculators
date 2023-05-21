42c10ea83d80e3033bf9ca067fdbfaa4e8061028
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2023-05-21 15:25:48 -0700

    add equation print outs

--- random-calculators/2022-2023/arc_la.py
+++ random-calculators/2022-2023/arc_la.py
@@ -1,10 +1,18 @@
 from helper import get_sint, sint
 from math import pi
 
+
+
 while True:
-    r = get_sint("Radius")
-    a = get_sint("Arc Angle")
+    radius = get_sint("Radius")
+    ark_angle = get_sint("Arc Angle")
     
-    print("Lenght: ", sint((a * 2 * pi * r) / 360))
-    print("Area: ", sint((a * pi * (r ** 2)) / 360))
+    lenght = sint((ark_angle * 2 * pi * radius) / 360)
+    print("len / (2 * pi * %s) = %s / 360" % (radius, ark_angle))
+    print("Lenght: ", lenght)
+    
+    print("area / (pi * %s^2) = %s / 360" % (radius, ark_angle))
+    area = sint((ark_angle * pi * (radius ** 2)) / 360)
+    print("Area: ", area)
     print()
+
