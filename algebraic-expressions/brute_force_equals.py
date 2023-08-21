390f976f6da74bf519b6b9739b70820a3b9e8c3d
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2023-08-21 12:34:08 -0700

    use multithreading for bad solver

--- algebraic-expressions/brute_force_equals.py
+++ algebraic-expressions/brute_force_equals.py
@@ -1,62 +1,73 @@
 from algebraic_expression import Expression
+import multiprocessing
+from math import floor, ceil
 
-class ExpressionInputError(ValueError):
-    pass
+"""Mostly made just to learn multithreading in python, not meant to be useful. No real error handling"""
 
-min_possible = 0
-max_possible = 360
+leave_keywords = {"ex", "exit", "leave", "quit", "get me out of here!!!!!"}
 
-leave_keywords = {"ex", "exit", "leave", "quit", "get me out of here"}
+def type_input(type, prompt = "", default = None, *, end = ": "):
+    return type(input(prompt + f" (default {default})" + end) or default)
 
-print("Brute force problem solver")
+def find_value(expression: Expression, answer: int, min_possible: int, max_possible: int, num_of_decimals: int):
+    answer = round(answer, num_of_decimals)
+    for i in range(min_possible, max_possible):
+        if round(expression.set_equal(i), num_of_decimals) == answer:
+            return i
 
-print('Example input: "3x + 1 = 7"')
+def stared_find_value(args):
+    return find_value(*args)
 
-def set_eqaul(v: Expression, i: int|float) -> Expression:
-    return (v.set_eqaul(i) if v.unique_bases_count > 0 else v)
-
-while True:
+def main() -> None:
+    print("Welcome to brute force algebraic expression solver.")
+    print("Use to determine what number makes left side of equation work single variable expressions")
     print()
-    user_input = input("Enter question: ")
+    print("Example Input: x^2 + 2 = 27")
+    print("Example Output: x = 5")
+    print()
+    print("Please be specific on the following questions to avoid unnecessarily longer run time.")
 
-    if user_input in leave_keywords:
-        print("Bye Bye")
-        exit()
+    cpu_count = multiprocessing.cpu_count()
+    
+    going = True
 
-    try:
-        split_input = user_input.split("=")
-
-        if len(split_input) < 2:
-            raise ExpressionInputError("Problem must have left and right side seprated by equal sign")
-
-        vals = [Expression(v.strip()) for v in split_input]
-
-        if any(((v.unique_bases_count > 1) for v in vals)):
-            raise ExpressionInputError("All expression must be monomials (only have 1 varible)")
+    while going:
+        print()
+        user_input = input("Enter equation: ")
+        
+        if user_input in leave_keywords:
+            print("Bye Bye")
+            exit(0)
             
-
-        for v in vals:
-            try:
-                var = v._get_base
-                break
-            except Exception:
-                continue
-        else:
-            raise ExpressionInputError("Must have at lease 1 variable")
+        expression, answer = user_input.split("=")
+        expression = Expression(expression)
+        answer = float(answer)
+        
+        min_possible = type_input(float, "What is the minimum possible number it could be", -1000)
+        max_possible = type_input(float, "What is the maximum number it could be", 1000)
+        num_of_decimals = type_input(int, "How many decimals places", 0)
+    
+        confirm = input(f"Warning: this program will use all {cpu_count} of your cores. Start (Y/n): ").lower().strip()
+        
+        if confirm == "n":
+            print("Cancelling...")
+            continue
+        
+        chunk_size = ceil((max_possible - min_possible) / cpu_count)
+        
+        expression_var_value = None
+        
+        with multiprocessing.Pool(cpu_count) as pool:
+            outputs = pool.imap_unordered(stared_find_value, [(expression, answer, start_val, start_val + chunk_size, num_of_decimals) for start_val in range(floor(min_possible), ceil(max_possible), chunk_size)])
             
+            while expression_var_value is None:
+                expression_var_value = next(outputs)
+                if expression_var_value is not None:
+                    print("while loop found")
+            
+            pool.terminate()
 
-        if any(((v.unique_bases_count == 1 and v._get_base != var) for v in vals)):
-            raise ExpressionInputError("Can only have one varible accros all expression")
+        print(expression_var_value)
 
-        compare_expression, *expressions = vals
-
-        for i in range(min_possible, max_possible+1):
-            compare = set_eqaul(compare_expression, i)
-
-            if all((set_eqaul(v, i) == compare for v in expressions)):
-                print(f"{var} = {i}")
-                break
-        else:
-            print(f"No valid value for {var}")
-    except ExpressionInputError as ex:
-        print(ex)
+if __name__ == "__main__":
+    main()
